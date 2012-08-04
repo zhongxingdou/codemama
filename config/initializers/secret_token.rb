cf3e3104deac916d7a12d836cf132f4bf50d18d1
zhongxingdou <zhongxingdou@gmail.com>
2012-08-04 21:20:04 +0800

    added all files, gem device generated views and model.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codemama::Application.config.secret_token = '0de2134b566c5babb9c1d2d8905523cc83838cbca1021b360944eaa81921cc491fba7921c117ca6e224f3458e9a255461007f642436008c52924793c1f8854e0'
