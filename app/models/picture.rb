466ca662294f9b1d6bc8394095077708563955ee
zhongxingdou <zhongxingdou@gmail.com>
2012-08-09 13:38:29 +0800

    删除图片信息记录时也删除存储在硬盘上的文件

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,4 +4,18 @@ class Picture < ActiveRecord::Base
   validates :user_id, presence: true
 
   belongs_to :user
+
+  before_destroy :rm_pic
+
+
+  SaveRoot = "public/imgs"
+
+  def rm_pic
+    name = self.name
+    subdir = name[0...name.index("_")]
+    fullname = SaveRoot + "/" + subdir + "/" + name
+    if File.exists? fullname
+      FileUtils.rm(fullname)
+    end
+  end
 end
