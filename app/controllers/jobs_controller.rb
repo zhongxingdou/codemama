891fa081872bc07501be25f632a7c35bc06c5081
zhongxingdou <zhongxingdou@gmail.com>
2013-06-04 15:03:48 +0800

    updated:增加返回单个招聘岗位的action

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,7 @@ class JobsController < InheritedResources::Base
   # before_filter :authenticate_user!, :set_name
 
   def index
-    @jobs = Job.paginate(:page => params[:page], :per_page => 10).order("id DESC")
+    @jobs = Job.select("position, location, created_at, expired_date").paginate(:page => params[:page], :per_page => 10).order("id DESC")
     @count = Job.count
     result = {
       :jobs => @jobs,
@@ -15,6 +15,18 @@ class JobsController < InheritedResources::Base
         format.js  { render  :json => result.to_json, :callback => params[:callback] }
       else
         format.json  { render  :json => @jobs.to_json }
+      end
+    end
+  end
+
+
+  def show
+    @job = Job.find(params[:id])
+    respond_to do |format|
+      if params[:callback]
+        format.js  { render  :json => @job, :callback => params[:callback] }
+      else
+        format.json  { render  :json => @job.to_json }
       end
     end
   end
